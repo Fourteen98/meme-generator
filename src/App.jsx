9c7f0a80b800fcc0e02b58f7dab7f376e38b4472
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-14 15:44:22 +0000

    fix use state

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 import Header from './components/Header'
 import Meme from './components/Meme'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,42 +1,45 @@
 import React from "react";
 import memesData from "../memesData";
-import { useState } from "react";
+import {useState} from "react";
 
 function Meme() {
-    const [memeUrl, setMemeUrl] = useState(()=> "");
-    let url
-    function getMemeImage(){
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        url = memesArray[randomNumber].url
-        setMemeUrl(() => memesArray[randomNumber].url)
-    }
-    const memeContainerStyle = {
-      backgroundImage: `url(${memeUrl})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      borer: "1px solid red",
-      width: "100%",
-      height: "450px",
-      border: "1px solid red",
-    }
 
-    return (
-        <main>
-            <div className="container">
-            <form className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Buttom text"/>
-                <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
-            </form>
-              <div className="meme--container" style={memeContainerStyle}>
+  const [memeUrl, setMemeUrl] = useState(() => "" );
+  const [memeHeight, setMemeHeight] = useState(() => "" );
+  const [memeWidth, setMemeWidth] = useState(() => "" );
 
-              </div>
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setMemeUrl(() => memesArray[randomNumber].url)
+    setMemeHeight(() => memesArray[randomNumber].height)
+    setMemeWidth(() => memesArray[randomNumber].width)
+    event.preventDefault()
+    console.log(randomNumber)
+  }
 
-            </div>
-        </main>
-    )
+  const memeStyle = {
+    height: memeHeight,
+    width: memeWidth,
+    "object-fit": "contain"
+  }
+
+  return (
+    <main>
+      <div className="container">
+        <form className="form">
+          <input type="text" className="form--input" placeholder="Top text"/>
+          <input type="text" className="form--input" placeholder="Buttom text"/>
+          <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
+        </form>
+        <div className="meme--container">
+          <img src={memeUrl} alt="meme" style={memeStyle} className="meme--image"/>
+
+        </div>
+
+      </div>
+    </main>
+  )
 }
 
 export default Meme;
